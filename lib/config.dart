5f60ac6af330e7e4202e25baa273ed2a5c61949d
yukopratama <yukofland@gmail.com>
2022-12-03 09:27:16 +0800

    save

--- lib/config.dart
+++ lib/config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   //# CONFIG
-  static String yourName = "DenyOcr";
-  static String phoneNumber = "082146727433";
+  static String yourName = "YUKO";
+  static String phoneNumber = "082220460808";
   //# ---------------------------------------
   //# ---------------------------------------
 
